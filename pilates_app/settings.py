878803dfe528f91945564be48cfbdc9dccf7876b
GonzaViceWork <gonzavice.work@gmail.com>
2025-02-03 14:08:56 -0500

    Change settings.py for production

--- pilates_app/settings.py
+++ pilates_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-22op6q(r4%wg7x2zx(=he4bf7p!)_mn+_%2lo5uf4y6k*id*xw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['your-backend.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -80,7 +81,7 @@ WSGI_APPLICATION = 'pilates_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(os.getenv('RENDER_DB_DIR', BASE_DIR), 'db.sqlite3'),
     }
 }
 
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
